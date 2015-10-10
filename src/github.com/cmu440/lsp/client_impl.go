62d4bb190ba79b12670697034e0b20f1d1a744dc
Fan Xia <frankxia0404@gmail.com>
2015-10-10 01:06:26 -0400

    Completed checkpoint 1

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,22 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/cmu440/lspnet"
+)
 
 type client struct {
-	// TODO: implement this!
+	connID         int
+	nextSeqNum     int
+	connectChan    chan bool
+	writeChan      chan []byte
+	sendChan       chan []byte
+	receiveChan    chan Message
+	readBufferChan chan Message
+	conn           *lspnet.UDPConn
+	addr           *lspnet.UDPAddr
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -19,21 +31,113 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+	raddr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := lspnet.DialUDP("udp", nil, raddr)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &client{
+		connectChan:    make(chan bool),
+		writeChan:      make(chan []byte),
+		sendChan:       make(chan []byte),
+		receiveChan:    make(chan Message),
+		readBufferChan: make(chan Message),
+		nextSeqNum:     1,
+		conn:           conn,
+		addr:           raddr,
+	}
+
+	go client.writeToServer()
+	go client.readFromServer()
+	go client.handleConnection()
+
+	return client.connect()
+}
+
+func (c *client) connect() (Client, error) {
+	msg := NewConnect()
+	ltrace.Println("Attempt to connect")
+	buf, _ := json.Marshal(msg)
+	c.writeChan <- buf
+	for _ = range c.connectChan {
+		return c, nil
+	}
+	return nil, errors.New("Connection closed")
+}
+
+func (c *client) writeToServer() {
+	for msg := range c.sendChan {
+		c.conn.Write(msg)
+	}
+}
+
+func (c *client) readFromServer() {
+	var msg Message
+	buf := make([]byte, BUFFER_SIZE)
+	for {
+		n, err := c.conn.Read(buf)
+		if err != nil {
+			ltrace.Println(err)
+			continue
+		}
+
+		json.Unmarshal(buf[:n], &msg)
+		ltrace.Println("Receive message: ", msg.String())
+
+		c.receiveChan <- msg
+	}
+}
+
+func (c *client) handleConnection() {
+	for {
+		select {
+		case msg := <-c.receiveChan:
+			switch msg.Type {
+			case MsgAck:
+				if msg.SeqNum == 0 {
+					c.connID = msg.ConnID
+					c.connectChan <- true
+				}
+			case MsgData:
+				c.readBufferChan <- msg
+				ackMsg := NewAck(msg.ConnID, msg.SeqNum)
+				ltrace.Println("Send message:", ackMsg)
+				buf, _ := json.Marshal(ackMsg)
+				c.sendChan <- buf
+			}
+		case msg := <-c.writeChan:
+			c.sendChan <- msg
+		}
+	}
 }
 
 func (c *client) ConnID() int {
-	return -1
+	return c.connID
 }
 
 func (c *client) Read() ([]byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return nil, errors.New("not yet implemented")
+	if msg, open := <-c.readBufferChan; open {
+		return msg.Payload, nil
+	} else {
+		return nil, errors.New("Channel closed")
+	}
 }
 
 func (c *client) Write(payload []byte) error {
-	return errors.New("not yet implemented")
+	// TODO add hash
+	msg := NewData(c.connID, c.nextSeqNum, payload, nil)
+	ltrace.Println("Send message: ", msg)
+
+	buf, _ := json.Marshal(msg)
+	c.nextSeqNum++
+	c.writeChan <- buf
+	return nil
+
 }
 
 func (c *client) Close() error {
--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -2,10 +2,41 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/cmu440/lspnet"
+	"strconv"
+	"log"
+	"os"
+)
+
+const (
+	BUFFER_SIZE = 1024
+	INIT_SEQ    = -1
+)
+
+var ltrace *log.Logger = log.New(os.Stderr, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+type clientInfo struct {
+	connId      int
+	nextSeqNum  int
+	clientAddr  *lspnet.UDPAddr
+	receiveChan chan Message
+	sendChan    chan Message
+}
 
 type server struct {
-	// TODO: implement this!
+	serverConn            *lspnet.UDPConn
+	clientConnChan        chan *lspnet.UDPAddr
+	serverReadChan        chan Message
+	serverWriteChan       chan Message
+	serverWriteOkChan     chan bool
+	serverCloseConnChan   chan int
+	serverCloseConnOkChan chan bool
+	clients               map[int]*clientInfo
+	count                 int
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -15,21 +46,154 @@ type server struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	return nil, errors.New("not yet implemented")
+	laddr, err := lspnet.ResolveUDPAddr("udp", lspnet.JoinHostPort("localhost", strconv.Itoa(port)))
+	if err != nil {
+		return nil, err
+	}
+
+	serverConn, err := lspnet.ListenUDP("udp", laddr)
+	s := &server{
+		serverConn:            serverConn,
+		clientConnChan:        make(chan *lspnet.UDPAddr),
+		serverReadChan:        make(chan Message),
+		serverWriteChan:       make(chan Message),
+		serverWriteOkChan:     make(chan bool),
+		serverCloseConnChan:   make(chan int),
+		serverCloseConnOkChan: make(chan bool),
+		clients:               make(map[int]*clientInfo),
+		count:                 0,
+	}
+	go s.readFromClients()
+	go s.handleServerEvents()
+	return s, nil
+}
+
+func (s *server) readFromClients() {
+	buf := make([]byte, BUFFER_SIZE)
+	var msg Message
+	for {
+		n, clientAddr, err := s.serverConn.ReadFromUDP(buf)
+		if err != nil {
+			ltrace.Println(err)
+			return
+		}
+
+		json.Unmarshal(buf[:n], &msg)
+		ltrace.Println(msg.String())
+
+		switch msg.Type {
+		case MsgConnect:
+			s.clientConnChan <- clientAddr
+		default:
+			if c, ok := s.clients[msg.ConnID]; ok {
+				c.receiveChan <- msg
+			}
+		}
+	}
+}
+
+func (s *server) handleServerEvents() {
+	for {
+		select {
+		case clientAddr := <-s.clientConnChan:
+			s.addClient(clientAddr)
+		case msg := <-s.serverWriteChan:
+			if c, ok := s.clients[msg.ConnID]; ok {
+				s.serverWriteOkChan <- true
+				if msg.SeqNum == INIT_SEQ {
+					msg.SeqNum = c.nextSeqNum
+					c.nextSeqNum++
+				}
+				c.sendChan <- msg
+			} else {
+				s.serverWriteOkChan <- false
+			}
+		case connId := <-s.serverCloseConnChan:
+			if _, ok := s.clients[connId]; ok {
+				s.serverCloseConnOkChan <- true
+				delete(s.clients, connId)
+			} else {
+				s.serverCloseConnOkChan <- false
+			}
+		}
+	}
+}
+
+func (s *server) addClient(clienAddr *lspnet.UDPAddr) {
+	c := clientInfo{
+		connId:      s.count,
+		nextSeqNum:  1,
+		clientAddr:  clienAddr,
+		receiveChan: make(chan Message),
+		sendChan:    make(chan Message),
+	}
+	s.clients[c.connId] = &c
+	s.count++
+	print("New connection: ", c.connId)
+
+	go s.handleClientEvents(c.connId)
+
+	c.sendChan <- *NewAck(c.connId, 0)
+}
+
+func (s *server) handleClientEvents(connId int) {
+	c := s.clients[connId]
+	for {
+		select {
+		case msg := <-c.sendChan:
+			ltrace.Println("Sending:", msg.String())
+			buf, _ := json.Marshal(msg)
+			s.serverConn.WriteToUDP(buf, c.clientAddr)
+		case msg := <-c.receiveChan:
+			switch msg.Type {
+			case MsgData:
+				s.serverReadChan <- msg
+				ackMsg := NewAck(msg.ConnID, msg.SeqNum)
+				buf, _ := json.Marshal(ackMsg)
+				ltrace.Println("Sending: ", msg.String())
+				s.serverConn.WriteToUDP(buf, c.clientAddr)
+			}
+		}
+	}
 }
 
 func (s *server) Read() (int, []byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return -1, nil, errors.New("not yet implemented")
+	if msg, open := <-s.serverReadChan; open {
+		return msg.ConnID, msg.Payload, nil
+	} else {
+		return -1, nil, errors.New("Channel closed.")
+	}
 }
 
 func (s *server) Write(connID int, payload []byte) error {
-	return errors.New("not yet implemented")
+	// TODO add hash
+	msg := NewData(connID, INIT_SEQ, payload, nil)
+
+	s.serverWriteChan <- *msg
+	if ok, open := <-s.serverWriteOkChan; open {
+		switch ok {
+		case true:
+			return nil
+		case false:
+			return errors.New(fmt.Sprintf("connID does not exist: %v", connID))
+		}
+	}
+
+	return errors.New("Channel closed")
 }
 
 func (s *server) CloseConn(connID int) error {
-	return errors.New("not yet implemented")
+	s.serverCloseConnChan <- connID
+	if ok, open := <-s.serverCloseConnOkChan; open {
+		switch ok {
+		case true:
+			return nil
+		case false:
+			return errors.New(fmt.Sprintf("connID does not exist: %v", connID))
+		}
+	}
+
+	return errors.New("Channel closed")
 }
 
 func (s *server) Close() error {
